eb679444021e4daed22f7a8654639b6dfe523ada
Manav Manish Parmar <mmparmar044@gmail.com>
2024-11-03 16:48:24 +0530

    Assignment 4 dbms part 2 added

--- DBMS/04.sql
+++ DBMS/04.sql
@@ -22,3 +22,59 @@ DELIMITER ;
 call area_proc();
 
 SELECT * from circle;
+
+--Borrower Fine
+
+CREATE TABLE borrower(
+    roll INT PRIMARY KEY NOT NULL,
+    name VARCHAR(20),
+    doi DATE,
+    book_name VARCHAR(20),
+    status VARCHAR(1)
+);
+
+CREATE TABLE fine(
+    roll INT,
+    dor DATE,
+    fine_amount INT,
+    FOREIGN KEY(roll) REFERENCES borrower(roll)
+);
+
+INSERT INTO borrower (roll, name, doi, book_name, status) VALUES
+(1, 'Amit', '2024-05-29', 'FDS', 'I'),
+(2, 'Sita', '2024-07-21', 'OOP', 'I'),
+(3, 'Ravi', '2024-07-09', 'PHY', 'I');
+
+DELIMITER $$
+
+CREATE PROCEDURE fill_fine(IN roll_no INT,IN name VARCHAR(20))
+BEGIN
+DECLARE today DATE;
+DECLARE issueDate DATE;
+DECLARE fine_amt INT;
+DECLARE diff INT;
+
+SET today = CURDATE();
+SELECT doi INTO issueDate FROM borrower WHERE roll_no = roll;
+SET diff = DATEDIFF(today,issueDate);
+IF diff > 15 AND diff <= 30 THEN
+    SET fine_amt = diff*3;
+    INSERT INTO fine(roll,dor,fine_amount) VALUES(roll_no,today,fine_amt);
+ELSEIF diff>30 THEN
+    SET fine_amt = diff*15;
+    INSERT INTO fine(roll,dor,fine_amount) VALUES(roll_no,today,fine_amt);
+ELSE
+    SET fine_amt = 0;
+END IF;
+
+UPDATE borrower SET status = 'R' where roll = roll_no;
+END $$
+
+DELIMITER ;
+
+CALL fill_fine(1, 'FDS');
+CALL fill_fine(2, 'OOP');
+CALL fill_fine(3, 'PHY');
+
+SELECT * FROM fine;
+
